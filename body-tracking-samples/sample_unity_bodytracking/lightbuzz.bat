b09ff1fdd9e0f0e3c24bb7b095bd4039e83af733
Sei Kang <yhs7409@naver.com>
2026-01-06 21:07:26 +0900

    Feat: Add Save Joint CSV

---
+++ body-tracking-samples/sample_unity_bodytracking/lightbuzz.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+xcopy "E:\Azure-Kinect-Samples\body-tracking-samples\sample_unity_bodytracking\Assets\LightBuzz_Azure_Kinect\Plugins\Windows\Common\directml.dll" "C:\Program Files\Unity\Hub\Editor\2022.3.56f1\Editor" /Y
